b9c3611442016850f720c9611013a4cc5af67247
Anthony Davi <ant.davi07@gmail.com>
2021-03-22 18:56:30 -0300

    firt commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# ImersãoDev - Conversor de moedas - 001
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/antdavi/pen/YzNzXRE](https://codepen.io/antdavi/pen/YzNzXRE).
+
+
---
+++ dist/script.js
@@ -0,0 +1,9 @@
+function converter() {
+  alert("converteu")
+}
+
+//var temperaturaEmCelsius = parseFloat(prompt("Qual a temperatura em Celsius(°C)"))
+
+//temperaturaEmKelvin = temperaturaEmCelsius + 273.15
+
+//alert(temperaturaEmCelsius + " °C em Kelvin é " + temperaturaEmKelvin.toFixed(2) + " K")
